94199770a31f775acb3a268f879ffb5d3cb62436
Levi A <anderzonz23@hotmail.com>
2020-11-02 15:26:19 -1000

    Created Issue class and added all properties

---
+++ P5/Issue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5
+{
+    class Issue
+    {
+        class Issue
+        {
+            public int ID { get; }
+            public int ProjectId { get; set; }
+            public string Title { get; set; }
+            public DateTime Discoverer { get; set; }
+            public string InitialDescription { get; set; }
+            public string Component { get; set; }
+            public int IssueStatusId { get; set; }
+        }
+    }
+}
+}
