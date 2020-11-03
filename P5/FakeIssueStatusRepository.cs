3432a69b0525c887492a2d0e539b3a61527b6d96
Levi A <anderzonz23@hotmail.com>
2020-11-02 17:52:06 -1000

    Created IssueStatus class and added properties Created IIssueStatusRepository interface and added prototype methods Created FakeIssueStatusRepository class and added prototype method implementations

---
+++ P5/FakeIssueStatusRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5
+{
+    public class FakeIssueStatusRepository : IIssueStatusRepository
+    {
+        private static List<IssueStatus> IssueStatuses = new List<IssueStatus>();
+        void Add(int Id, string value)
+        {
+
+        }
+        List<IssueStatus> GetAll()
+        {
+            return IssueStatuses;
+        }
+        int GetIdByStatus(string value)
+        {
+            return 0;
+        }
+        string GetValueById(int Id)
+        {
+            return "";
+        }
+
+    }
+}
